982087d8d7e2229666fedea8e780324269f92827
MCXSL <lo_ken@bk.ru>
2026-05-11 18:15:30 +0300

    Добавьте файлы проекта.

---
+++ VediYchanikov/header/Ве_крест_крест.h
@@ -0,0 +1,185 @@
+﻿#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#ifndef Байкал
+
+#define енто =
+#ifdef Русская_арифметика
+
+#define равен ==
+#define плюсплюс ++
+#define минусминус --
+
+#define больше >
+#define меньше <
+#define большеРавно >=
+#define меньшеРавно =<
+
+#define плюс +
+#define минус -
+#define умножить *
+#define делить /
+
+#endif
+
+#define царь_батюшка_главный int main
+#define внедрить using
+#define Русь std
+#define молвить cout
+#define внемлить cin
+#define воздать return
+
+#ifdef _WIN32
+#include <Windows.h>
+#define вперёд_славяне SetConsoleOutputCP(1251)
+#else
+#define вперёд_славяне ;
+#endif
+
+#define пахать do
+#define покуда while
+#define для for
+
+#define коли if
+#define отнюдь else
+#define правда true
+#define ложь false
+
+#define династия class
+#define семейство struct
+#define перепись enum
+
+#define бить_ящеров break
+#define добить_ящеров continue
+#define путевой_камень switch
+#define путь case
+#define базированно default
+
+#define правда true
+#define кривда false
+
+#define прыг_скок endl
+#define новь new
+#define казнь delete
+#define туда_не_знаю_куда nullptr
+#define НИЧТО NULL
+#define судьба random
+#define приказ_княжий const
+#define мощь pow
+#define получи_басурман throw
+#define гнев_Перуна exception
+#define читать_летопись ifstream
+#define писать_летопись ofstream
+#define летопись fstream
+#define друже friend
+#define окстись goto
+#define свет_мой_зеркальце template
+#define скажи typename
+#define царский private
+#define народный public
+#define дружинный protected
+#define встрой inline
+#define кощей static
+#define однобокий unsigned
+#define пытать_ящера try
+#define поймать_ящера catch
+#define хутор namespace
+#define откупорить open
+#define закупорить close
+#define суд_Перуна auto
+#define аки static_cast
+#define очами_стрельнуть peek
+
+#define зачерпнуть getline
+#define добрый_молодец good
+#define змей_подколодный fail
+#define вот_и_сказочке_конец eof
+#define вск eof
+#define приток_Байкала ostream
+#define отток_Байкала istream
+#define меняло std::swap
+#define забор std::vector
+#define отщипнуть get
+
+внедрить целина енто int;
+внедрить вель_дробь енто double;
+внедрить малый_дробь енто float;
+внедрить буква енто char;
+внедрить много_букав енто Русь::string;
+внедрить бестолочь енто void;
+внедрить карлик енто short;
+внедрить долговязый енто long;
+внедрить Петр_Первый енто long;
+внедрить мерило енто size_t;
+внедрить розсуд енто bool;
+
+#define отцепить begin
+#define прекратить end
+#define скопировать copy
+#define отсортировать sort
+#define найти find
+#define удалить erase
+#define вставить insert
+#define слияние merge
+#define сравнить compare
+#define максимум max_element
+#define минимум min_element
+#define развернуть reverse
+#define перемешать shuffle
+#define аналогичен Русь::equal
+#define размер size
+#define начало start
+#define конец end
+
+#define вконец push_back
+#define убратьПоследний pop_back
+
+//add cpp files and rename it
+
+#include "сторонний_код/сортировка.cpp"
+
+#define пузырьки bubbleSort
+#define внедрение insertionSort
+#define быстрая quickSort
+
+// для сортировки векторов: отсортировать(вектор.начало(), вектор.конец())
+
+//культурно скопированный текст из других форков
+
+// автор: teslaproduuction
+#define бардак cerr
+#define хаос clog
+#define необъятный_бардак wcerr
+#define необъятный_хаос wclog
+#define внемлить_ящерский wcin
+#define молвить_ящерский wcout
+
+// только для linux систем
+
+#ifndef _WIN32
+
+#include <unistd.h>
+
+#define запуск exec*
+#define ожидать sleep
+#define чуть_ожидать usleep
+
+#endif
+
+// необязательная часть
+
+#ifdef _дополнения
+
+#define ж ;
+#define ЮЮ >>
+#define ББ <<
+
+#endif
+
+перепись счёт_древних_русов{ ноль енто 0, целковый енто 1, полушка енто 2, четвертушка енто 3, осьмушка енто 4,
+пудовичок енто 5, медячок енто 6, серебрячок енто 7, золотничок енто 8, девятичок енто 9, десятичок енто 10 };
+
+#endif
